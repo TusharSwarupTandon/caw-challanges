4b2c8e76ce02b5c9ccc251a12efac0668d83c595
Tushar Swarup Tandon <tushar.tandon118@gmail.com>
2022-06-16 16:50:24 +0530

    prettier

--- Challange4/index.js
+++ Challange4/index.js
@@ -1,42 +1,43 @@
-import {selectRandomKey, keyToBePressed} from './utilities.js'
+import { selectRandomKey, keyToBePressed } from "./utilities.js";
 
 //main keyboard
-const keyboard = document.querySelectorAll('button');
+const keyboard = document.querySelectorAll("button");
 
 //variable to hold which key is currently jiggling
 let currentJigglingKey;
 
-const startKeyJiggle = (key) =>
-{
-    //set the current jiggling key as the key
-    currentJigglingKey = key;
-    //start key's jiggling
-    key.classList.add('jiggle');
-}
+const startKeyJiggle = (key) => {
+  //set the current jiggling key as the key
+  currentJigglingKey = key;
+  //start key's jiggling
+  key.classList.add("jiggle");
+};
 
-const stopKeyJiggle = (key) =>
-{
-    //stop key's jiggling
-    key.classList.remove('jiggle');
-}
+const stopKeyJiggle = (key) => {
+  //stop key's jiggling
+  key.classList.remove("jiggle");
+};
 
-const changeJigglingKey = () =>
-{
-    //stop old key jiggling
-    stopKeyJiggle(currentJigglingKey);
-    //start new key's jiggle
-    startKeyJiggle(selectRandomKey()); 
-}
+const changeJigglingKey = () => {
+  //stop old key jiggling
+  stopKeyJiggle(currentJigglingKey);
+  //start new key's jiggle
+  startKeyJiggle(selectRandomKey());
+};
 
-document.onkeyup = (pressedKey) =>
-{  
-    //if current pressed key is same as pressed key
-    //jiggle new key
-    if(pressedKey.key.toUpperCase() === keyToBePressed())
-        changeJigglingKey();
-}
+document.onkeyup = (pressedKey) => {
+  //if current pressed key is same as pressed key
+  //jiggle new key
+  if (pressedKey.key.toUpperCase() === keyToBePressed()) changeJigglingKey();
+};
 
 //start key jiggling
 startKeyJiggle(selectRandomKey());
 
-export {keyboard, currentJigglingKey, startKeyJiggle, stopKeyJiggle, changeJigglingKey}
+export {
+  keyboard,
+  currentJigglingKey,
+  startKeyJiggle,
+  stopKeyJiggle,
+  changeJigglingKey,
+};
--- Challange4/utilities.js
+++ Challange4/utilities.js
@@ -1,27 +1,28 @@
-import {currentJigglingKey, keyboard} from './index.js'
+import { currentJigglingKey, keyboard } from "./index.js";
 
 //get total number of keys in the keyboard
-const getNumberOfKeys = () => 
-{
-    return keyboard.length; //53
-}
+const getNumberOfKeys = () => {
+  return keyboard.length; //53
+};
 
 //generate random number between 0 and total number of keys
-const generateRandomNumber = (numberOfKeys) =>
-{
-    return Math.floor(Math.random() * numberOfKeys);
-}
+const generateRandomNumber = (numberOfKeys) => {
+  return Math.floor(Math.random() * numberOfKeys);
+};
 
 //select the random key
-const selectRandomKey = () =>
-{
-    return keyboard[generateRandomNumber(getNumberOfKeys())];
-}
+const selectRandomKey = () => {
+  return keyboard[generateRandomNumber(getNumberOfKeys())];
+};
 
 //the key which should be pressed to stop jiggling
-const keyToBePressed = () =>
-{
-    return currentJigglingKey.getAttribute('data-key').toUpperCase();
-}
+const keyToBePressed = () => {
+  return currentJigglingKey.getAttribute("data-key").toUpperCase();
+};
 
-export {getNumberOfKeys, generateRandomNumber, selectRandomKey, keyToBePressed}
+export {
+  getNumberOfKeys,
+  generateRandomNumber,
+  selectRandomKey,
+  keyToBePressed,
+};

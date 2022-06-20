fc138d3b0361991cc5f46033d3f977d6b99b4aa2
Tushar Swarup Tandon <tushar.tandon118@gmail.com>
2022-06-20 14:22:18 +0530

    adding prettier config file

--- Challange1/booleanChecking.js
+++ Challange1/booleanChecking.js
@@ -1,57 +1,57 @@
-import { getMinuteTextField, getSecondTextField } from "./getElements.js";
-import { startButton } from "./index.js";
+import { getMinuteTextField, getSecondTextField } from './getElements.js';
+import { startButton } from './index.js';
 
 // check if the input is an integer
 const isInteger = (number) => {
-  // check if it is a number and does not contain decimal
-  if (!Number.isNaN(number) && number.indexOf(".") < 0) {
-    return true;
-  }
-  return false;
+    // check if it is a number and does not contain decimal
+    if (!Number.isNaN(number) && number.indexOf('.') < 0) {
+        return true;
+    }
+    return false;
 };
 
 // check is the given time is between 0 and 59
 const isTimeinRange = (time) => {
-  if (time <= 59 && time >= 0) {
-    return true;
-  }
+    if (time <= 59 && time >= 0) {
+        return true;
+    }
 };
 
 // check if time is currently in editing state
 const isEditingTime = () => {
-  // check if seconds text field is currenty is editing state
-  // if yes return true
-  // else return false
-  const secondTextField = getSecondTextField();
-  return !secondTextField.disabled ? true : false;
+    // check if seconds text field is currenty is editing state
+    // if yes return true
+    // else return false
+    const secondTextField = getSecondTextField();
+    return !secondTextField.disabled ? true : false;
 };
 
 // check if entered time value is valid
 const checkValidTime = () => {
-  // get minute and seconds value
-  const minutes = getMinuteTextField().value;
-  const seconds = getSecondTextField().value;
-  // minutes and seconds should be between 59 and 0
-  // also it should be an integer, no decimal values allowed
-  if (isInteger(minutes) && isInteger(seconds)) {
-    if (isTimeinRange(minutes) && isTimeinRange(seconds)) {
-      return true;
+    // get minute and seconds value
+    const minutes = getMinuteTextField().value;
+    const seconds = getSecondTextField().value;
+    // minutes and seconds should be between 59 and 0
+    // also it should be an integer, no decimal values allowed
+    if (isInteger(minutes) && isInteger(seconds)) {
+        if (isTimeinRange(minutes) && isTimeinRange(seconds)) {
+            return true;
+        }
     }
-  }
-  getMinuteTextField().value = "15";
-  getSecondTextField().value = "00";
-  return false;
+    getMinuteTextField().value = '15';
+    getSecondTextField().value = '00';
+    return false;
 };
 
 // Check if timer is running
 const isTimerRunning = () => {
-  // if start button text is stop timer is running
-  // return true
-  // otherwise return false
-  if (startButton.innerHTML === "stop") {
-    return true;
-  }
-  return false;
+    // if start button text is stop timer is running
+    // return true
+    // otherwise return false
+    if (startButton.innerHTML === 'stop') {
+        return true;
+    }
+    return false;
 };
 
 export { isEditingTime, checkValidTime, isTimerRunning };
